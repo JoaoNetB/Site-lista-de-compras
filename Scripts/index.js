94814ca281db27881a3975edae235253259eb415
João Bevolo <netojb156@gmail.com>
2021-05-15 23:35:36 -0300

    added responsiveness on the add page and a new script for the delete function

--- Scripts/index.js
+++ Scripts/index.js
@@ -3,6 +3,17 @@ function stateDelete(divDelete) {
 
     if (displayDelete == "none") {
         document.getElementById(divDelete).style.display = "block";
+    } else if (displayDelete == "block") {
+        document.getElementById(divDelete).style.display = "none";
     }
 }
 
+function alternatives(valor) {
+
+    if (valor == "yes") {
+        alert("Esse conteúdo foi apagado")
+        stateDelete('choose-delete');
+    } else if (valor == "no") {
+        stateDelete('choose-delete');
+    }
+}
